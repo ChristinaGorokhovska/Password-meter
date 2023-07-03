09d01dba2955d052a404eeb668278b9b77949820
ChristinaGorokhovska <kristinagorokhovska@gmail.com>
2023-07-03 12:53:27 +0200

    spaces in password

--- src/utilities/checkStrength.ts
+++ src/utilities/checkStrength.ts
@@ -1,6 +1,6 @@
 export const checkStrength = (password: string) => {
   if (!password.length) return 0;
-  if (password.trim().length < 8 || /\s/.test(password)) return -1;
+  if (password.trim().length < 8 || /^\s|\s$/.test(password)) return -1;
 
   const conditions: RegExp[] = [/[a-zA-Z]/g, /\d/, /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/];
   const matches = conditions.map((reg: RegExp) => reg.test(password)).filter((item: boolean) => item == true);
